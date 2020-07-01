c01ae92e3e99cdc66583ba2f3e4a14f7954063a6
Rocky Racoon <mdoig5247@gmail.com>
2020-07-01 03:11:11 -0500

    started writing regular cypress tests, needed some package.json updates

---
+++ cypress/integration/TraditionalTestsV1/cypressTestV1.js
@@ -0,0 +1,103 @@
+const route = '/gridHackathonV1.html';
+
+describe('Task 1', () => {
+  beforeEach(() => {
+    cy.visit(route);
+  });
+
+  it('checks nav visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#NAV__colxlcollg__10').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#NAV__colxlcollg__10').should('not.be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#NAV__colxlcollg__10').should('not.be.visible');
+    };
+  });
+
+  it('checks wishlist button visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#LI____51').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#LI____51').should('not.be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#LI____51').should('not.be.visible');
+    };
+  });
+
+  it('checks cart badge visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#STRONG____50').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#STRONG____50').should('be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#STRONG____50').should('not.be.visible');
+    };
+  });
+
+  it('checks search bar visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#INPUTtext____42').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#INPUTtext____42').should('be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#INPUTtext____42').should('not.be.visible');
+    };
+  });
+
+  it('checks filter options sidebar visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#ASIDE__collg__67').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#ASIDE__collg__67').should('not.be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#ASIDE__collg__67').should('not.be.visible');
+    };
+  });
+
+  it('checks grid view icons visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#LI____200').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#LI____200').should('not.be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#LI____200').should('not.be.visible');
+    };
+  });
+
+  it('checks filter text visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#SPAN____208').should('not.be.visible');
+    cy.viewport(768, 700);
+    cy.get('#SPAN____208').should('be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#SPAN____208').should('not.be.visible');
+    };
+  });
+
+  it('checks filter icon visibility', () => {
+    cy.viewport(1200, 700);
+    cy.get('#ti-filter').should('be.visible');
+    cy.viewport(768, 700);
+    cy.get('#ti-filter').should('not.be.visible');
+    if (Cypress.browser.name === 'chrome') {
+      cy.viewport(500, 700);
+      cy.get('#ti-filter').should('not.be.visible');
+    };
+  });
+
+  // it('checks product grid row count', () => {
+    // TK
+  // });
+
+  // it('checks item quick options visibility', () => {
+    // TK
+  // });
+});
