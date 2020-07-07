78d9583e3235c688a2e2b78e9a2802b2a89ee039
Rocky Racoon <mdoig5247@gmail.com>
2020-07-06 23:41:19 -0500

    added param descriptions and comments to commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,34 +1,21 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
+// 
+// All four custom commands accept the same parameters.
+// @param taskNumber -- the number of the hackathon task
+// @param testName -- name of the test
+// @param domId -- id of the element being tested
+// @param viewportWidth -- sets the viewport width for the correct type of test
+// @param device -- laptop, tablet, or mobile; choose depending on viewportWidth (laptop is 1200, tablet is 768, mobile is 500)
+// @param arrayName -- name of the array to which every result is pushed 
+// 
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('isVisible', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
   let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
-  arrayName.push(result);
+  arrayName.push(result); // initially push 'fail' result to array that will hold all results
   cy.viewport(viewportWidth, 700);
   cy.get(`#${domId}`).should('be.visible').then(() => {
+    // if assertion passes, then 'fail' result is popped from array and replaced with a 'pass' result
     arrayName.pop();
     result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
     arrayName.push(result);
