0480f8d00afe3e3ad18c4c5855bb0c21156337b5
Rocky Racoon <mdoig5247@gmail.com>
2020-07-07 22:59:12 -0500

    added code and results for v2 task 3 ✅; did small alphabetizing for v2 test spec

--- cypress/integration/TraditionalTestsV2/cypressTestV2.js
+++ cypress/integration/TraditionalTestsV2/cypressTestV2.js
@@ -1,5 +1,5 @@
-const route = '/gridHackathonV2.html';
 const results = [];
+const route = '/gridHackathonV2.html';
 
 describe('Traditional V2 Test Task 1', () => {
   beforeEach(() => {
@@ -214,6 +214,176 @@ describe('Traditional V2 Test Task 2', () => {
   it('checks second of correct products appear in filter results 500x700 (chrome only)', () => {
     if (Cypress.browser.name === 'chrome') {
       cy.isVisible(2, 'filter results visibility', 'DIV__colcolmd__236', 500, 'mobile', results);
+    };
+  });
+});
+
+describe('Traditional V2 Test Task 3', () => {
+  beforeEach(() => {
+    cy.visit(route);
+    cy.contains('a', 'Appli Air x Night').click();
+  });
+
+  after(() => {
+    for (let i = 0; i < results.length; i++) {
+      cy.readFile('Traditional-V2-TestResults.txt').then(data => {
+        cy.writeFile('Traditional-V2-TestResults.txt', `${data}${results[i]}`);
+      });
+    }
+  });
+
+  // every command gets its own `it` to ensure every test runs even if another fails
+  it('checks product name visibility and accuracy 1200x700', () => {
+    cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 1200, 'laptop', results, 'have.text', 'Appli Air x Night');
+  });
+
+  it('checks product name visibility and accuracy 768x700', () => {
+    cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 768, 'tablet', results, 'have.text', 'Appli Air x Night');
+  });
+
+  it('checks product name visibility and accuracy 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 500, 'mobile', results, 'have.text', 'Appli Air x Night');
+    };
+  });
+
+  it('checks product image visibility 1200x700', () => {
+    cy.checkAttr(3, 'product image visibility', 'shoe_img', 1200, 'laptop', results, 'have.attr', 'style');
+  });
+
+  it('checks product image visibility 768x700', () => {
+    cy.checkAttr(3, 'product image visibility', 'shoe_img', 768, 'table', results, 'have.attr', 'style');
+  });
+
+  it('checks product image visibility 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.checkAttr(3, 'product image visibility', 'shoe_img', 500, 'mobile', results, 'have.attr', 'style');
+    };
+  });
+
+  it('checks product rating visibility 1200x700', () => {
+    cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 1200, 'laptop', results);
+  });
+
+  it('checks product rating visibility 768x700', () => {
+    cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 768, 'tablet', results);
+  });
+
+  it('checks product rating visibility 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 500, 'mobile', results);
+    };
+  });
+
+  it('checks product sku visibility and accuracy 1200x700', () => {
+    cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 1200, 'laptop', results, 'have.text', 'SKU: MTKRY-001');
+  });
+
+  it('checks product sku visibility and accuracy 768x700', () => {
+    cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 768, 'tablet', results, 'have.text', 'SKU: MTKRY-001');
+  });
+
+  it('checks product sku visibility and accuracy 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 500, 'mobile', results, 'have.text', 'SKU: MTKRY-001');
+    };
+  });
+
+  it('checks size selector visibility 1200x700', () => {
+    cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 1200, 'laptop', results);
+  });
+
+  it('checks size selector visibility 768x700', () => {
+    cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 768, 'tablet', results);
+  });
+
+  it('checks size selector visibility 500x700', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 500, 'mobile', results);
+    };
+  });
+
+  it('checks size selector default option accuracy 1200x700', () => {
+    cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 1200, 'laptop', results, 'have.text', 'Small (S)');
+  });
+
+  it('checks size selector default option accuracy 768x700', () => {
+    cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 768, 'tablet', results, 'have.text', 'Small (S)');
+  });
+
+  it('checks size selector default option accuracy 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 500, 'mobile', results, 'have.text', 'Small (S)');
+    };
+  });
+
+  it('checks quantity selector visibility 1200x700', () => {
+    cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 1200, 'laptop', results);
+  });
+
+  it('checks quantity selector visibility 768x700', () => {
+    cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 768, 'tablet', results);
+  });
+
+  it('checks quantity selector visibility 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 500, 'mobile', results);
+    };
+  });
+
+  it('checks price visibility and accuracy 1200x700', () => {
+    cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 1200, 'laptop', results, 'have.text', '$33.00');
+  });
+
+  it('checks price visibility and accuracy 768x700', () => {
+    cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 768, 'tablet', results, 'have.text', '$33.00');
+  });
+
+  it('checks price visibility and accuracy 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 500, 'mobile', results, 'have.text', '$33.00');
+    };
+  });
+
+  it('checks old price visibility and accuracy 1200x700', () => {
+    cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 1200, 'laptop', results, 'have.text', '$48.00');
+  });
+
+  it('checks old price visibility and accuracy 768x700', () => {
+    cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 768, 'tablet', results, 'have.text', '$48.00');
+  });
+
+  it('checks old price visibility and accuracy 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 500, 'mobile', results, 'have.text', '$48.00');
+    };
+  });
+
+  it('checks discount visibility 1200x700', () => {
+    cy.isVisible(3, 'discount visibility', 'discount', 1200, 'laptop', results);
+  });
+
+  it('checks discount visibility 768x700', () => {
+    cy.isVisible(3, 'discount visibility', 'discount', 768, 'tablet', results);
+  });
+
+  it('checks discount visibility 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'discount visibility', 'discount', 500, 'mobile', results);
+    };
+  });
+
+  it('checks add to cart button visibility 1200x700', () => {
+    cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 1200, 'laptop', results);
+  });
+  
+  it('checks add to cart button visibility 768x700', () => {
+    cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 768, 'tablet', results);
+  });
+
+  it('checks add to cart button visibility 500x700 (chrome only)', () => {
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 500, 'mobile', results);
     };
   });
 });
