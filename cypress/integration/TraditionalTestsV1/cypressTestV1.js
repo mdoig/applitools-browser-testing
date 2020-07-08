42336036dfc048129624018eb7ce7c054e8d0c8a
Rocky Racoon <mdoig5247@gmail.com>
2020-07-07 21:46:09 -0500

    added code and results for v1 task 3 test ✅; alphabetized consts in v1 spec and custom commands

--- cypress/integration/TraditionalTestsV1/cypressTestV1.js
+++ cypress/integration/TraditionalTestsV1/cypressTestV1.js
@@ -1,5 +1,5 @@
-const route = '/gridHackathonV1.html';
 const results = [];
+const route = '/gridHackathonV1.html';
 
 describe('Traditional V1 Test Task 1', () => {
   beforeEach(() => {
@@ -128,6 +128,109 @@ describe('Traditional V1 Test Task 2', () => {
     if (Cypress.browser.name === 'chrome') {
       cy.isVisible(2, 'filter results visibility', 'DIV__colcolmd__210', 500, 'mobile', results);
       cy.isVisible(2, 'filter results visibility', 'DIV__colcolmd__235', 500, 'mobile', results);
+    };
+  });
+});
+
+describe('Traditional V1 Test Task 3', () => {
+  beforeEach(() => {
+    cy.visit(route);
+    cy.contains('a', 'Appli Air x Night').click();
+  });
+
+  after(() => {
+    for (let i = 0; i < results.length; i++) {
+      cy.readFile('Traditional-V1-TestResults.txt').then(data => {
+        cy.writeFile('Traditional-V1-TestResults.txt', `${data}${results[i]}`);
+      });
+    }
+  });
+
+  it('checks product name visibility and accuracy', () => {
+    cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 1200, 'laptop', results, 'have.text', 'Appli Air x Night');
+    cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 768, 'tablet', results, 'have.text', 'Appli Air x Night');
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'product name visibility and accuracy', 'shoe_name', 500, 'mobile', results, 'have.text', 'Appli Air x Night');
+    };
+  });
+
+  it('checks product image visibility', () => {
+    cy.checkAttr(3, 'product image visibility', 'shoe_img', 1200, 'laptop', results, 'have.attr', 'style');
+    cy.checkAttr(3, 'product image visibility', 'shoe_img', 768, 'table', results, 'have.attr', 'style');
+    if (Cypress.browser.name === 'chrome') {
+      cy.checkAttr(3, 'product image visibility', 'shoe_img', 500, 'mobile', results, 'have.attr', 'style');
+    };
+  });
+
+  it('checks product rating visibility', () => {
+    cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 1200, 'laptop', results);
+    cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 768, 'tablet', results);
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'product rating visibility', 'SPAN__rating__76', 500, 'mobile', results);
+    };
+  });
+
+  it('checks product sku visibility and accuracy', () => {
+    cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 1200, 'laptop', results, 'have.text', 'SKU: MTKRY-001');
+    cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 768, 'tablet', results, 'have.text', 'SKU: MTKRY-001');
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'product sku visibility and accuracy', 'SMALL____84', 500, 'mobile', results, 'have.text', 'SKU: MTKRY-001');
+    };
+  });
+
+  it('checks size selector visibility', () => {
+    cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 1200, 'laptop', results);
+    cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 768, 'tablet', results);
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'size selector visibility', 'DIV__row__88', 500, 'mobile', results);
+    };
+  });
+
+  it('checks size selector default option accuracy', () => {
+    cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 1200, 'laptop', results, 'have.text', 'Small (S)');
+    cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 768, 'tablet', results, 'have.text', 'Small (S)');
+    if (Cypress.browser.name === 'chrome') {
+      cy.checkAttr(3, 'size selector default option accuracy', 'OPTION____94', 500, 'mobile', results, 'have.text', 'Small (S)');
+    };
+  });
+
+  it('checks quantity selector visibility', () => {
+    cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 1200, 'laptop', results);
+    cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 768, 'tablet', results);
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'quantity selector visibility', 'DIV__row__98', 500, 'mobile', results);
+    };
+  });
+
+  it('checks price visibility and accuracy', () => {
+    cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 1200, 'laptop', results, 'have.text', '$33.00');
+    cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 768, 'tablet', results, 'have.text', '$33.00');
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'price visibility and accuracy', 'new_price', 500, 'mobile', results, 'have.text', '$33.00');
+    };
+  });
+
+  it('checks old price visibility and accuracy', () => {
+    cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 1200, 'laptop', results, 'have.text', '$48.00');
+    cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 768, 'tablet', results, 'have.text', '$48.00');
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisibleCheckAttr(3, 'old price visibility and accuracy', 'old_price', 500, 'mobile', results, 'have.text', '$48.00');
+    };
+  });
+  
+  it('checks discount visibility', () => {
+    cy.isVisible(3, 'discount visibility', 'discount', 1200, 'laptop', results);
+    cy.isVisible(3, 'discount visibility', 'discount', 768, 'tablet', results);
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'discount visibility', 'discount', 500, 'mobile', results);
+    };
+  });
+
+  it('checks add to cart button visibility', () => {
+    cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 1200, 'laptop', results);
+    cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 768, 'tablet', results);
+    if (Cypress.browser.name === 'chrome') {
+      cy.isVisible(3, 'add to cart button visibility', 'DIV__btnaddtoca__113', 500, 'mobile', results);
     };
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 // ***********************************************
 // 
-// All five custom commands accept the same parameters.
+// All seven custom commands accept these parameters:
 // @param taskNumber -- the number of the hackathon task
 // @param testName -- name of the test
 // @param domId -- id of the element being tested
@@ -8,14 +8,29 @@
 // @param device -- laptop, tablet, or mobile; choose depending on viewportWidth (laptop is 1200, tablet is 768, mobile is 500)
 // @param arrayName -- name of the array to which every result is pushed 
 // 
+// Two custom commands, `checkAttr` and `isVisibleCheckAttr`, accept these additional parameters:
+// @param chainer -- any valid chainer that Cypress accepts
+// @param value -- value to assert against chainer
+//
 // ***********************************************
 
-Cypress.Commands.add('isVisible', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
+Cypress.Commands.add('checkAttr', (taskNumber, testName, domId, viewportWidth, device, arrayName, chainer, value) => {
   let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
   arrayName.push(result); // initially push 'fail' result to array that will hold all results
   cy.viewport(viewportWidth, 700);
-  cy.get(`#${domId}`).should('be.visible').then(() => {
+  cy.get(`#${domId}`).should(chainer, value).then(() => {
     // if assertion passes, then 'fail' result is popped from array and replaced with a 'pass' result
+    arrayName.pop();
+    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
+    arrayName.push(result);
+  });
+});
+
+Cypress.Commands.add('filterResultsNumbers', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
+  let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
+  arrayName.push(result);
+  cy.viewport(viewportWidth, 700);
+  cy.get(`#${domId}`).children().should('have.length', 2).then(() => {
     arrayName.pop();
     result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
     arrayName.push(result);
@@ -33,20 +48,25 @@ Cypress.Commands.add('isNotVisible', (taskNumber, testName, domId, viewportWidth
   });
 });
 
-Cypress.Commands.add('productGridVisible', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
+Cypress.Commands.add('isVisible', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
   let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
   arrayName.push(result);
   cy.viewport(viewportWidth, 700);
-  cy.get(`#${domId}`).children().then((divs) => {
-    for (let i = 0; i < divs.length; i++) {
-      cy.get(divs[i]).within(() => {
-        cy.get('ul').should('be.visible').then(() => {
-          arrayName.pop();
-          result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
-          arrayName.push(result);
-        });
-      });
-    }
+  cy.get(`#${domId}`).should('be.visible').then(() => {
+    arrayName.pop();
+    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
+    arrayName.push(result);
+  });
+});
+
+Cypress.Commands.add('isVisibleCheckAttr', (taskNumber, testName, domId, viewportWidth, device, arrayName, chainer, value) => {
+  let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
+  arrayName.push(result);
+  cy.viewport(viewportWidth, 700);
+  cy.get(`#${domId}`).should('be.visible').should(chainer, value).then(() => {
+    arrayName.pop();
+    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
+    arrayName.push(result);
   });
 });
 
@@ -67,35 +87,19 @@ Cypress.Commands.add('productGridNotVisible', (taskNumber, testName, domId, view
   });
 });
 
-Cypress.Commands.add('filterResultsNumbers', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
+Cypress.Commands.add('productGridVisible', (taskNumber, testName, domId, viewportWidth, device, arrayName) => {
   let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
   arrayName.push(result);
   cy.viewport(viewportWidth, 700);
-  cy.get(`#${domId}`).children().should('have.length', 2).then(() => {
-    arrayName.pop();
-    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
-    arrayName.push(result);
-  });
-});
-
-Cypress.Commands.add('isVisibleCheckAttr', (taskNumber, testName, domId, viewportWidth, device, arrayName, chainer, value) => {
-  let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
-  arrayName.push(result);
-  cy.viewport(viewportWidth, 700);
-  cy.get(`#${domId}`).should('be.visible').should(chainer, value).then(() => {
-    arrayName.pop();
-    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
-    arrayName.push(result);
-  });
-});
-
-Cypress.Commands.add('checkAttr', (taskNumber, testName, domId, viewportWidth, device, arrayName, chainer, value) => {
-  let result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Fail\n`;
-  arrayName.push(result);
-  cy.viewport(viewportWidth, 700);
-  cy.get(`#${domId}`).should(chainer, value).then(() => {
-    arrayName.pop();
-    result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
-    arrayName.push(result);
+  cy.get(`#${domId}`).children().then((divs) => {
+    for (let i = 0; i < divs.length; i++) {
+      cy.get(divs[i]).within(() => {
+        cy.get('ul').should('be.visible').then(() => {
+          arrayName.pop();
+          result = `Task: ${taskNumber}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${Cypress.browser.name}, Viewport: ${viewportWidth} x 700, Device: ${device}, Status: Pass\n`;
+          arrayName.push(result);
+        });
+      });
+    }
   });
 });
